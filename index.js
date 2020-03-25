e68102bf0c5b21d42d01b4e945fbbaca6725b0da
TaiwoSeun <seunemmanueltaiwo@gmail.com>
2020-03-25 03:46:42 +0100

    index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+//the varaiable below stores my name
+let name = 'Taiwo';
+//the courses i offer is store in an array
+let course = ['Html', 'Css', 'JavaScript'];
+//this displays my name and courses
+console.log('my name is Taiwo and my courses are Html, Css, and JavaScript');
+//this loops through and gives odd numbers from 1-200
+for (let i = 0; i <= 200; i++){
+    if (i % 2 !==0) console.log(i);
+    }
